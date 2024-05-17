2ca24c6fa6638f130fc0a4d3a5ecb3ec604ec7bd
slizer98 <erick.m.villa.r@gmail.com>
2024-05-17 13:29:46 -0600

    refactor: Update App component to include weather map image and title

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import styles from './App.module.css'
 import Form from './components/form/Form'
+import mapa from '../public/map.png'
+import styles from './App.module.css'
 
 function App() {
 
   return (
     <>
       <div className={styles.container}>
+        <div>
+          <h1 className={styles.title}>Aplicación de clima</h1>
+          <figure className={styles.figure}>
+            <img src={mapa} alt="" />
+          </figure>
+        </div>
         <Form />
-        <p>2</p>
       </div>
     </>
   )
